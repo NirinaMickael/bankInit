c11342b4f9f39d67bba7e05adceb47f6131418a2
Ravelson-Sarobidy <ravelsonsarobidy7@gmail.com>
2022-04-12 15:03:33 +0200

    nouveau component sidemenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PageLayoutModule } from './pages/page-layout/page-layout.module';
+import { SidemenuComponent } from './features/sidemenu/sidemenu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidemenuComponent
   ],
   imports: [
     BrowserModule,
