e7829c4fa8af5e0c959d83feba00894fa56e1255
Nirinamickael <todisoanirinamickael@gmail.com>
2022-05-03 20:12:48 +0300

    add themes in shared folder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PageLayoutComponent } from './pages/page-layout/page-layout.component';
+import { NotFoundComponent } from './shared/pages/not-found/not-found.component';
 
 const routes: Routes = [
   
@@ -9,6 +10,10 @@ const routes: Routes = [
     path: 'pages',
     loadChildren: () => import('./pages/page-layout/page-layout.module').then(m => m.PageLayoutModule),
     component:PageLayoutComponent
+  },
+  {
+    path:"**",
+    component : NotFoundComponent
   }
 ];
 
